490e80f832223f64b9f801e5bc0293bf7770c8a9
Bpetrick <170433522+BenjPetr@users.noreply.github.com>
2026-03-17 08:33:57 +0000

    feat: initial push

--- src/dc_plus/interfaces/type_hints.py
+++ src/dc_plus/interfaces/type_hints.py
@@ -4,3 +4,11 @@
 # If a copy of the MPL was not distributed with this file,
 # you can obtain one at https://mozilla.org/MPL/2.0/.
 # Mozilla Public License, version 2.0
+
+"""Type hints for DC+ interfaces."""
+
+import typing
+
+from jaxtyping import Int
+
+PosInt: typing.TypeAlias = Int
--- src/dc_plus/numpy/__init__.py
+++ src/dc_plus/numpy/__init__.py
@@ -4,3 +4,9 @@
 # If a copy of the MPL was not distributed with this file,
 # you can obtain one at https://mozilla.org/MPL/2.0/.
 # Mozilla Public License, version 2.0
+
+"""The numpy implementation for DC+.
+
+This implementation is intended for reference and testing purposes only.
+It is currently not optimized for performance, but for readability.
+"""
